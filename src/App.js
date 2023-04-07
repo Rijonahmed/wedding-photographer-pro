2abd928b5f6a20f12cec849939cd50d52cd5b1e0
Rijonahmed <rijon036@gmail.com>
2023-04-07 23:44:33 +0600

    h2 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
      <div>
       <p>This is a wedding photographer pro new project</p>
+      <h2>Git hub install</h2>
      </div>
     </div>
   );
