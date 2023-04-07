f363fa388ddc01c8c4f1c27f6a5eb22c92a5f6e4
Rijonahmed <rijon036@gmail.com>
2023-04-07 23:41:11 +0600

    wedding new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <p>This is a wedding photographer pro new project</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
